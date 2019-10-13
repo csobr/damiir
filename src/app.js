38e55d6cd90174c0407a68649c21e8017feb776c
Siham <siham.hadi@live.se>
2019-10-13 17:25:01 +0200

    Updated files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello world!</h1>
+    
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
-const HTMLWebPackPlugin = require ("html-webpack-plugin");
+
+const HtmlWebPackPlugin = require ("html-webpack-plugin");
+
+
 module.exports = {
+    devtool: "source-map",
 
+    mode: 'development',
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+
+},
     module: {
-        rules: [
-           {
-               test: /\.js$/,
-               exclude: /(node_modules)/,
-               use: {
-                    loader: "babel-loader",
-                    options: {
-                    presets: [
-                        "@babel/preset-react", 
-                        "@babel/preset-env"
-                    ]},
-     } }]},
+        rules:[
+            
+            {
+                test: /\.(js|jsx)$/,
+                 exclude: /node_modules/, 
+                 loader: "babel-loader",
+        
+            }
+        ]},
+      
+    plugins: [
+    
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filname: "./index.html"
+        }),
+    
+  
+    ]
 
-plugins: [
-    new HTMLWebPackPlugin({
-        template: "./build/index.html",
-
-    })
-]};
+};
