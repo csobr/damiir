ab4cb5da028e4f56d72ee1d6b168f94398a4ea0d
Siham <siham.hadi@live.se>
2019-10-19 21:25:33 +0200

    Updated files

--- src/components/home.js
+++ src/components/home.js
@@ -9,13 +9,20 @@ export default class Home extends Component {
       <div className="hero">
         <video className="videoTag" autoPlay loop muted>
           <source src={sample} type="video/mp4" />
-        </video>{" "}
+        </video>
         </div>      
-         <Globe />
-        <div className = "second--view">
+    
+        <div className = "second--view  second--view--one">
         <h2>“Look for systems, not heroes.”</h2>
-<p>― Hans Rosling, Factfulness: Ten Reasons We're Wrong About the World—and Why Things Are Better Than You Think</p>
- 
+<p className = "qoute">― Hans Rosling, Factfulness: Ten Reasons We're Wrong About the World—and Why Things Are Better Than You Think</p></div>
+  <div className = "second--view second--view--two ">
+  {/* <Globe />  */}
+  <span className = "circle"></span>
+<div className = "mission">  
+<h3>Mission</h3>
+
+        <p className = "mission--text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
         </div>
       </div>
     );
