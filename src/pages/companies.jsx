27e697bd7eb1f34dabe04c72d5e8b2b781cc0f4a
Siham <siham.hadi@live.se>
2019-12-13 11:34:38 +0100

    display company cards from json

--- src/pages/companies.jsx
+++ src/pages/companies.jsx
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
-import data from "../pages/data/companiesList.json";
+import companies from "../pages/data/companiesList.json";
 export default class Companies extends Component {
-  render() {
-  return (
-    <div className="companies--wrapper">
-      {data.map((company, index) => {
-        return (
-          <div key={index}> 
-        <div className="cards">
-          <div className="card">
-            <button id="company--popup">
-              <span></span>
-            </button>
+                 renderCardData() {
+            return companies.map((company, index) => {
+              const { id, name, url, logo, description } = company;
+              return (
+                <div key={id}>
+          
+                  <div className="card">
+                    <button id="company--popup">
+                      <span></span>
+                    </button>
+                    <div className="circle--companay">
+                      <img
+                        src={logo}
+                        className="company--logo"
+                        alt="company logo"
+                      ></img>
+                    </div>
+                    <div className="company--text company--text__preview">
+                      {description}
+                    </div>
+                    </div>
+                    </div>
+              );
+            });
+                 }
 
-            <div className="circle--companay">
-              <img
-                src={company.logo}
-                className="company--logo"
-                alt="company logo"
-              ></img>
-            </div>
-            <div className="company--text company--text__preview">{company.description}
-            </div>
-          </div>
-            </div>
-            </div>
-      )})};
-    </div>
-  );
-  }
-  }
+                 render() {
+                   return (
+                     <div className="companies--wrapper">
+                       <div className = "cards">
+                             {this.renderCardData()}
+                       </div>
+                       </div>
+           
+
+
+                   );
+                 }
+               }
