5d560f91180d41526d8f8448a0cee2eabc21a362
Siham <siham.hadi@live.se>
2019-12-30 13:13:21 +0100

    css grid

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 // import { useLocation} from "react-router"
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from './components/navigation.jsx'
-import Home from './pages/home.jsx'
-import Companies from './pages/companies.jsx'
+import Navbar from "./components/navigation.jsx";
+import Home from "./pages/home.jsx";
+import Companies from "./pages/companies.jsx";
 import About from "./pages/about.jsx";
-import Footer from './components/footer.jsx'
+import Footer from "./components/footer.jsx";
 
-function App () {
+function App() {
+  return (
+    <div className="site--wrapper">
+      <Router>
+        <Navbar />
+        <content>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/companies" component={Companies} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </content>
+      </Router>
 
-        return (
-          <div className="site--wrapper">
-            <Router>
-              <Navbar />
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/companies" component={Companies} />
-                <Route path="/about" component={About} />
-              </Switch>
-            </Router>
-            <Footer />
-          </div>
-        );
-    
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
